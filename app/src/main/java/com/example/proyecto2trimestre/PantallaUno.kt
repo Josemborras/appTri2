7c000936439bd89cf6d64f486ca018f7b1bcad29
Josemborras <josemanuel.borras@escuelaestech.es>
2024-02-26 14:23:27 +0100

    empezando

---
+++ app/src/main/java/com/example/proyecto2trimestre/PantallaUno.kt
@@ -0,0 +1,26 @@
+package com.example.proyecto2trimestre
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PantallaUno : Fragment() {
+    private lateinit var binding: PantallaUnoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = PantallaUnoBinding.inflate(inflater, container, false)
+        return binding.root
+
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
